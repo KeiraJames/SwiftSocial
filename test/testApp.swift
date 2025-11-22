0632e1a651239faec9161c74c8714dea5a90dc78
Keira <keirajames2003@gmail.com>
2025-11-22 11:49:07 -0500

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Keira James on 11/22/25.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
